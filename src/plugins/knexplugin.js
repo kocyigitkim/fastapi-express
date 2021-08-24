916624f951c82d484fdf82e1d4bf9b2812028d07
Muhammed Koçyiğit <muhammetkocyigit0@gmail.com>
2021-08-24 17:02:14 +0300

    Knex stored procedure support added for mssql

--- src/plugins/knexplugin.js
+++ src/plugins/knexplugin.js
@@ -1,16 +1,31 @@
 const knex = require('knex');
-class KnexPlugin{
+
+async function KnexStoredProcedure(name, args) {
+    var objArgs = {};
+    for (var arg of args) {
+        objArgs[arg.name] = arg.value;
+    }
+    return (await this.raw(`exec ${name} ${args.map(arg => `:${arg.name}`).join(", ")}`, objArgs).catch(console.error)) || [];
+}
+async function KnexStoredProcedureJSON(name, args) {
+    var response = (await this.raw(`exec ${name} ?`, JSON.stringify(args)).catch(console.error)) || [];
+    return JSON.parse(response.map(item => Object.values(item)[0]).join(""));
+}
+
+class KnexPlugin {
     /**
      * 
      * @param {knex.Knex.Config} knexConfig 
      * @param {String} name 
      */
-    constructor(knexConfig, name = "db"){
+    constructor(knexConfig, name = "db") {
         this.name = name;
         this.knexConfig = knexConfig;
         this.db = knex(knexConfig);
+        this.db.storedProcedure = KnexStoredProcedure.bind(this.db);
+        this.db.storedProcedureJSON = KnexStoredProcedureJSON.bind(this.db);
     }
-    getterMethod(){
+    getterMethod() {
         return this.db;
     }
 }
